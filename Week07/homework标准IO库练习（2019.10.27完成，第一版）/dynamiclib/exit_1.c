b78d50c6d3b1336428f5d87464d41a8e1c426c9d
Custavo <1142166194@qq.com>
2019-11-04 01:06:39 -0800

    rm Week07

--- Week07/homework标准IO库练习（2019.10.27完成，第一版）/dynamiclib/exit_1.c
+++
@@ -1,9 +0,0 @@
-#include "head.h"
-int exit_1()
-{
-	system("clear");
-	printf("+--------------------------------------+\n");
-	printf("|               GoodBye!               |\n");
-	printf("+--------------------------------------+\n");
-	return 0;
-}
