01da447556cb89b0d40111b6ea0442f9864de88c
Custavo <1142166194@qq.com>
2019-11-11 00:49:58 -0800

    add Week10

---
+++ Week10/code/my.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <wait.h>
+#include <fcntl.h>
+#include <sys/stat.h>
